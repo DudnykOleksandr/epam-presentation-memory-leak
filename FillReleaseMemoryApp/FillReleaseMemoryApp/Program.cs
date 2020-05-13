2cf9f6ef494d9a7f3188b17cba1c5af677599f1c
Oleksandr Dudnyk <dudniksasha@gmail.com>
2020-05-13 15:05:36 +0300

    init

---
+++ FillReleaseMemoryApp/FillReleaseMemoryApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FillReleaseMemoryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do
+            {
+
+                var array = new double[100000000];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = i;
+                }
+
+                Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+                Console.WriteLine("*");
+
+            } while (true);
+        }
+    }
+}
